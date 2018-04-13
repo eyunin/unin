1e0b01a44127beb485800280968516098773c1fb
eyunin <yunin92@gmail.com>
2018-04-13 23:04:46 +0300

    ver 2.0 added: new blocks, new features, mobile version

--- index.php
+++ index.php
@@ -1,13 +1,299 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Мастерская по ремонту обуви в Запорожье</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 	<link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'];?>/assets/css/main.css">
-	<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+	<link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'];?>/assets/css/slick.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<h1>UNIN - Мастерская по ремонту обуви в Запорожье</h1>
-	<h3>Подзаголовок</h3>
+	<?php include 'preloader.php'; ?>
+	<input type="checkbox" id="hmt" class="hidden-menu-ticker">
+	<label class="btn-menu" for="hmt">
+		<span class="first"></span>
+		<span class="second"></span>
+		<span class="second"></span>
+		<span class="third"></span>
+	</label>
+	<div class="alpha-block"></div>
+	<div class="wrap">
+	<header id="header" class="header">
+		<a class="sml-logo" href="/"><img class="logo" src="/assets/img/logo-white.svg" alt="Обувная мастерская - Юнин А."></a>
+		<nav>
+			<ul class="nav">
+				<li><a href="/">Главная</a></li>
+				<li><a href="#about">О Нас</a></li>
+				<li class="logo-item"><a class="a-logo" href="/"><img class="logo" src="/assets/img/logo-white.svg" alt="Обувная мастерская - Юнин А."></a></li>
+				<li><a href="#price">Услуги</a></li>
+				<li><a href="#contact">Контакты</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="container-fluid">
+		<div class="main-slider">
+			<div class="slide">
+				наша мастерская заставит вашу обувь выглядеть лучше
+			</div>
+			<div class="slide">
+				доверьте ремонт вашей обуви профессионалам
+			</div>
+			<div class="slide">
+				восстановление вашей любимой обуви
+			</div>
+		</div>
+		<div class="content">
+			<div id="price" class="price">
+				<h1>Наши услуги</h1>
+				<div class="table-wrap">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Наименование работ</th>
+								<th>Количество</th>
+								<th>Стоимость, грн</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Набойки из полиуретана</td>
+								<td>пара</td>
+								<td>70</td>
+							</tr>
+							<tr>
+								<td>Набойки из резины, мужские/женские</td>
+								<td>пара</td>
+								<td>70/60</td>
+							</tr>
+							<tr>
+								<td>Набойки из металла</td>
+								<td>пара</td>
+								<td>70</td>
+							</tr>
+							<tr>
+								<td>Набойки мужские (фирменные)</td>
+								<td>пара</td>
+								<td>95</td>
+							</tr>
+							<tr>
+								<td>Косяки из полиуретана на носки</td>
+								<td>пара</td>
+								<td>50</td>
+							</tr>
+							<tr>
+								<td>Профилактика мужская фирм.</td>
+								<td>пара</td>
+								<td>95</td>
+							</tr>
+							<tr>
+								<td>Профилактика мужская рант.</td>
+								<td>пара</td>
+								<td>80</td>
+							</tr>
+							<tr>
+								<td>Профилактика женская фирм.</td>
+								<td>пара</td>
+								<td>95</td>
+							</tr>
+							<tr>
+								<td>Профилактика женская рант.</td>
+								<td>пара</td>
+								<td>80</td>
+							</tr>
+							<tr>
+								<td>Переклейка подошвы с прошивкой</td>
+								<td>пара</td>
+								<td>100</td>
+							</tr>
+							<tr>
+								<td>Подклейка подошвы</td>
+								<td>шт.</td>
+								<td>80</td>
+							</tr>
+							<tr>
+								<td>Установка латки с заправкой под подошву</td>
+								<td>шт.</td>
+								<td>40</td>
+							</tr>
+							<tr>
+								<td>Установка латки поверх обуви</td>
+								<td>шт.</td>
+								<td>40</td>
+							</tr>
+							<tr>
+								<td>Укрепление пяточной части верха обуви</td>
+								<td>пара</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td>Укрепление каблуков</td>
+								<td>шт.</td>
+								<td>40</td>
+							</tr>
+							<tr>
+								<td>Растяжка обуви</td>
+								<td>пара</td>
+								<td>60</td>
+							</tr>
+							<tr>
+								<td>Замена молнии (без стоимости молнии)</td>
+								<td>шт.</td>
+								<td>80</td>
+							</tr>
+							<tr>
+								<td>Замена собачки</td>
+								<td>шт.</td>
+								<td>30</td>
+							</tr>
+							<tr>
+								<td>Замена супинатора</td>
+								<td>шт.</td>
+								<td>80</td>
+							</tr>
+							<tr>
+								<td>Замена карманов из кожи</td>
+								<td>пара</td>
+								<td>70</td>
+							</tr>
+							<tr>
+								<td>Замена вкладной стельки из натуральной кожи</td>
+								<td>пара</td>
+								<td>70</td>
+							</tr>
+							<tr>
+								<td>Замена вкладной стельки из меха или байки</td>
+								<td>пара</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td>Замена каблуков женских / обтяжка</td>
+								<td>пара</td>
+								<td>250</td>
+							</tr>
+							<tr>
+								<td>Замена каблуков мужских наборных</td>
+								<td>пара</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td>Замена подошвы из кожволона</td>
+								<td>пара</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td>Замена подошвы ТЭП, полиуретан</td>
+								<td>пара</td>
+								<td>400</td>
+							</tr>
+							<tr>
+								<td>Замена резинки</td>
+								<td>пара</td>
+								<td>25</td>
+							</tr>
+							<tr>
+								<td>Замена основной стельки</td>
+								<td>пара</td>
+								<td>60</td>
+							</tr>
+							<tr>
+								<td>Замена каблуков резиновых</td>
+								<td>пара</td>
+								<td>85</td>
+							</tr>
+							<tr>
+								<td>Кожволон</td>
+								<td>пара</td>
+								<td>75</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div id="contact">
+			<div class="contacts">
+				<h1>Контактная информация</h1>
+				<div class="c-info">
+					<div class="ci-bl bt master">
+						<p><img src="/assets/img/contacts/master.svg" alt="Master">Мастер:</p>
+						<p>Александр Юнин</p>
+					</div>
+					<div class="ci-bl bt tel">
+						<p><img src="/assets/img/contacts/tel.svg" alt="Phone">Телефоны:</p>
+						<p><a href="tel:+380999365911">+380 99 936 5913</a></p>
+						<p><a href="tel:+380972758406">+380 97 275 8406</a></p>
+					</div>
+					<div class="ci-bl location">
+						<p><img src="/assets/img/contacts/location.svg" alt="Address">Адрес:</p>
+						<p>ул.Независимой Украины 90А, Запорожье, Украина</p>
+					</div>
+					<div class="ci-bl mail">
+						<p><img src="/assets/img/contacts/mail.svg" alt="Email">Email:</p>
+						<p><a href="mailto:unin@unin.zp.ua">unin@unin.zp.ua</a></p>
+					</div>
+					<div class="ci-bl w-100 social">
+						<a href="viber://add?number=380999365913" title="Viber"><img src="/assets/img/viber-logo.png" height="60" width="auto" alt="Viber"></a>
+						<a href="https://api.whatsapp.com/send?phone=380999365913" title="WhatsApp">
+							<img src="/assets/img/whatsapp-logo.svg" height="60" width="auto" alt="WhatsApp">
+						</a>
+						<p>Также вы можете связаться с нами в Viber и WhatsApp</p>
+					</div>
+				</div>
+			</div>
+			<div id="map"></div>
+		</div>
+		<footer class="footer">
+			<?php echo date("Y"); ?> | <a href="/">Обувная мастерская "Юнин"</a>
+		</footer>
+		<script>
+		function initMap() {
+			var uluru = {lat: 47.837910, lng: 35.144295};
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 17,
+				center: uluru
+			});
+			var image = '/assets/img/map_logo.png';
+			var marker = new google.maps.Marker({
+				position: uluru,
+				map: map,
+				icon: image
+			});
+			var goTo = 'https://goo.gl/maps/Z3jsv3ytpUR2';
+			var infoContent = 'Обувная мастерская "Юнин"';
+			var iWindow = new google.maps.InfoWindow({content: infoContent});
+			iWindow.open(map, marker);
+			marker.addListener('mouseover', function() {iWindow.open(map, marker);});
+			marker.addListener('click', function() {location = goTo});
+		}
+		</script>
+	</div>
+</div>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDgq5Pdi-skyqnnG-RNW4RuwM0sdep7hB4&callback=initMap" async defer></script>
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/slick.min.js"></script>
+<script>
+	$(document).ready(function(){
+		$('.main-slider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: true,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			centerPadding: '0',
+			focusOnSelect: true,
+			centerMode: true
+		});
+	});
+
+	$(document).on("scroll", function() {
+		if($(document).scrollTop()>100) {
+			$("#header").addClass("small");
+		} else {
+			$("#header").removeClass("small");
+		}
+	});
+</script>
 </body>
 </html>
---
+++ preloader.php
@@ -0,0 +1,171 @@
+<style>
+.preloader {
+	background: #262d39;
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	z-index: 100;
+	top: 0;
+}
+.cssload-loader {
+	position: relative;
+	width: 100px;
+	height: 100px;
+	-webkit-perspective: 780px;
+	perspective: 780px;
+}
+.cssload-loader::before {
+	content: "unin";
+	color: #fff;
+	position: absolute;
+	top: calc(50% - 21px);
+	left: 25%;
+	font-size: 21px;
+}
+
+.cssload-inner {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	box-sizing: border-box;
+	-o-box-sizing: border-box;
+	-ms-box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	border-radius: 50%;
+	-o-border-radius: 50%;
+	-ms-border-radius: 50%;
+	-webkit-border-radius: 50%;
+	-moz-border-radius: 50%;
+}
+
+.cssload-inner.cssload-one {
+	left: 0%;
+	top: 0%;
+	animation: cssload-rotate-one 1.15s linear infinite;
+	-o-animation: cssload-rotate-one 1.15s linear infinite;
+	-ms-animation: cssload-rotate-one 1.15s linear infinite;
+	-webkit-animation: cssload-rotate-one 1.15s linear infinite;
+	-moz-animation: cssload-rotate-one 1.15s linear infinite;
+	border-bottom: 3px solid #4c78ad;
+}
+
+.cssload-inner.cssload-two {
+	right: 0%;
+	top: 0%;
+	animation: cssload-rotate-two 1.15s linear infinite;
+	-o-animation: cssload-rotate-two 1.15s linear infinite;
+	-ms-animation: cssload-rotate-two 1.15s linear infinite;
+	-webkit-animation: cssload-rotate-two 1.15s linear infinite;
+	-moz-animation: cssload-rotate-two 1.15s linear infinite;
+	border-right: 3px solid #EBF0F6;
+}
+
+.cssload-inner.cssload-three {
+	right: 0%;
+	bottom: 0%;
+	animation: cssload-rotate-three 1.15s linear infinite;
+	-o-animation: cssload-rotate-three 1.15s linear infinite;
+	-ms-animation: cssload-rotate-three 1.15s linear infinite;
+	-webkit-animation: cssload-rotate-three 1.15s linear infinite;
+	-moz-animation: cssload-rotate-three 1.15s linear infinite;
+	border-top: 3px solid #c9e98a;
+}
+
+@keyframes cssload-rotate-one {
+	0% {
+		-webkit-transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+		transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+	}
+	100% {
+		-webkit-transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+		transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+	}
+}
+
+@-webkit-keyframes cssload-rotate-one {
+	0% {
+		-webkit-transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+	}
+	100% {
+		-webkit-transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+	}
+}
+
+@keyframes cssload-rotate-two {
+	0% {
+		-webkit-transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+		transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+	}
+	100% {
+		-webkit-transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+		transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+	}
+}
+
+@-webkit-keyframes cssload-rotate-two {
+	0% {
+		-webkit-transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+	}
+	100% {
+		-webkit-transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+	}
+}
+
+@keyframes cssload-rotate-three {
+	0% {
+		-webkit-transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+		transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+	}
+	100% {
+		-webkit-transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+		transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+	}
+}
+
+@-webkit-keyframes cssload-rotate-three {
+	0% {
+		-webkit-transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+	}
+	100% {
+		-webkit-transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+	}
+}
+</style>
+<div id="preloader" class="preloader">
+	<div class='cssload-loader'>
+		<div class='cssload-inner cssload-one'></div>
+		<div class='cssload-inner cssload-two'></div>
+		<div class='cssload-inner cssload-three'></div>
+	</div>
+</div>
+
+<script>
+	var preloader = document.getElementById("preloader");
+	function preloadFadeOut(el) {
+		el.style.opacity = 1;
+		var interpreloader = setInterval(function () {
+				el.style.opacity = el.style.opacity - 0.05;
+				if (el.style.opacity < 0.05) {
+					clearInterval(interpreloader);
+					preloader.parentNode.removeChild(preloader);
+				}
+			}, 16
+		);
+	}
+	function hidePreloader() {
+		setTimeout(function () {
+			preloadFadeOut(preloader);
+		}, 1500);
+	}
+	if (window.attachEvent) {
+		window.attachEvent('onload', hidePreloader);
+	} else if (window.addEventListener) {
+		window.addEventListener('load', hidePreloader, false);
+	} else {
+		document.addEventListener('load', hidePreloader, false);
+	}
+</script>
